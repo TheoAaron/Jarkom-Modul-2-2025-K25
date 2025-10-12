26c5917b5fcd1ca873b4d6c64739cfae03af1cab
TheoAaron <theodorusaaron31@gmail.com>
2025-10-12 22:05:46 +0800

    Fixed soal_2.sh to show output

--- script/soal_2.sh
+++ script/soal_2.sh
@@ -2,4 +2,8 @@
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.76.0.0/16
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE -s 10.76.0.0/
+
+# Test di semua Node
+
+ping google.com -c 2 # Tes Internet
